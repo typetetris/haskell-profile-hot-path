55d49158c29cb209edb0090a1c80379ceed20ac8
Eric Wolf <eric.wolf@lindenbaum.eu>
2022-02-24 09:36:03 +0100

    Cumulate ticks per cost_centre

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,12 @@ struct CostCentre {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+struct AnalysedCostCentre {
+    cost_centre: CostCentre,
+    ticks: u64,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 struct Profile {
     #[serde(rename(deserialize = "id"))]
     cost_centre: u64,
@@ -33,7 +39,7 @@ struct AnalysedProfile {
 }
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
-struct TopLevel<P> {
+struct TopLevel<P, C> {
     program: String,
     arguments: Vec<String>,
     rts_arguments: Vec<String>,
@@ -43,16 +49,20 @@ struct TopLevel<P> {
     total_ticks: u64,
     tick_interval: u32,
     total_alloc: u64,
-    cost_centres: Vec<CostCentre>,
+    cost_centres: Vec<C>,
     profile: P,
 }
 
-fn analyse_profile(input: Profile, cost_centres: &HashMap<u64, CostCentre>) -> AnalysedProfile {
+fn analyse_profile(
+    input: Profile,
+    cost_centres: &HashMap<u64, CostCentre>,
+    cost_centres_ticks: &mut HashMap<u64, u64>,
+) -> AnalysedProfile {
     let mut our_children: Vec<AnalysedProfile> = input
         .children
         .iter()
         .cloned()
-        .map(|c| analyse_profile(c, cost_centres))
+        .map(|c| analyse_profile(c, cost_centres, cost_centres_ticks))
         .collect();
     our_children.sort_by(|lhs, rhs| lhs.ticks_cumulative.cmp(&rhs.ticks_cumulative).reverse());
 
@@ -65,6 +75,11 @@ fn analyse_profile(input: Profile, cost_centres: &HashMap<u64, CostCentre>) -> A
         |c| (c.label.clone(), c.module.clone(), c.src_loc.clone()),
     );
 
+    {
+        let cost_centre_ticks = cost_centres_ticks.entry(input.cost_centre).or_insert(0);
+        *cost_centre_ticks += input.ticks;
+    }
+
     AnalysedProfile {
         label,
         module,
@@ -72,13 +87,33 @@ fn analyse_profile(input: Profile, cost_centres: &HashMap<u64, CostCentre>) -> A
         entries: input.entries,
         alloc: input.alloc,
         ticks: input.ticks,
-        ticks_cumulative: our_children.iter().map(|c| c.ticks_cumulative).sum::<u64>() + input.ticks,
+        ticks_cumulative: our_children.iter().map(|c| c.ticks_cumulative).sum::<u64>()
+            + input.ticks,
         children: our_children,
     }
 }
 
-fn analyse(input: TopLevel<Profile>) -> TopLevel<AnalysedProfile> {
-    let cost_centres: HashMap<u64, CostCentre> = input.cost_centres.iter().cloned().map(|c| (c.id, c)).collect();
+fn analyse(input: TopLevel<Profile, CostCentre>) -> TopLevel<AnalysedProfile, AnalysedCostCentre> {
+    let cost_centres: HashMap<u64, CostCentre> = input
+        .cost_centres
+        .iter()
+        .cloned()
+        .map(|c| (c.id, c))
+        .collect();
+    let mut cost_centres_ticks: HashMap<u64, u64> = HashMap::new();
+    let profile = analyse_profile(input.profile, &cost_centres, &mut cost_centres_ticks);
+    let mut cost_centres: Vec<AnalysedCostCentre> = input
+        .cost_centres
+        .into_iter()
+        .map(|cost_centre| {
+            let id = cost_centre.id;
+            AnalysedCostCentre {
+                cost_centre,
+                ticks: *cost_centres_ticks.get(&id).unwrap_or(&0),
+            }
+        })
+        .collect();
+    cost_centres.sort_by(|lhs, rhs| lhs.ticks.cmp(&rhs.ticks).reverse());
     TopLevel {
         program: input.program,
         arguments: input.arguments,
@@ -89,8 +124,8 @@ fn analyse(input: TopLevel<Profile>) -> TopLevel<AnalysedProfile> {
         total_ticks: input.total_ticks,
         tick_interval: input.tick_interval,
         total_alloc: input.total_alloc,
-        cost_centres: input.cost_centres,
-        profile: analyse_profile(input.profile, &cost_centres),
+        cost_centres,
+        profile,
     }
 }
 
